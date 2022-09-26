a82a7e7b8ad0b6f79d519885f40b31666fd929df
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-09-26 19:25:17 +0000

    adding quantity validation test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,7 +18,14 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
+    it 'should not validate without quantity' do
+      @product = Product.new(name: 'prickly', category: @category, quantity: nil, price_cents: 40)
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
+
+   end
 
 
 end
