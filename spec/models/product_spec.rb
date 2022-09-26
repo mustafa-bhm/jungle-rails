6118a3a3b464c9db92462ca03218d99e4735613d
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-09-26 19:27:02 +0000

    adding category validation test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -25,7 +25,16 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
-   end
+    it 'should not validate without category' do
+      @product = Product.new(name: 'prickly', category: nil, quantity: 2, price_cents: 40)
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+
+
+  end
 
 
 end
