a1c1e5f8055d520eb9c3fed897531652b4218658
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-09-26 21:49:21 +0000

    adding tests for users, validation & login cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates_uniqueness_of :email, case_sensitive: false, presence: true
+  validates :password, :password_confirmation, presence: true, length: { minimum: 3}
+
+  private
+
+  def self.authenticate_with_credentials(email, password)
+
+    if email == nil || password == nil
+      return false
+
+    else
+      user_email = email.downcase.strip
+
+      user = User.find_by_email(user_email)
+
+      user && user.authenticate(password) ? user : false
+    end
+
+  end
+
+
+
 end
