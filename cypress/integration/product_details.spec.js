af83acbd7611e53b6928958029f65ed0da06585f
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-09-27 16:13:31 +0000

    adding test to check product details

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,4 +1,4 @@
-describe("Navigation", () => {
+describe("Product details", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
@@ -7,5 +7,10 @@ describe("Navigation", () => {
   });
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
+  });
+  it("should get product details", () => {
+    cy.get(".products article").last().click();
+    cy.get(".product-detail").should("be.visible");
+    cy.get(".product-detail h1").should("have.text", "Giant Tea");
   });
 });
