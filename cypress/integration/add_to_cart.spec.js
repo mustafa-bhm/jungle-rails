bd9d7af4c1134dbc3cc211b12ed9432e034468d8
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-09-27 16:41:01 +0000

    adding test for cart

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,4 +1,4 @@
-describe("Navigation", () => {
+describe("Add to cart ", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
@@ -7,5 +7,9 @@ describe("Navigation", () => {
   });
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
+  });
+  it("Clicks on add to cart", () => {
+    cy.get("button").contains("Add").click({ force: true });
+    cy.get("a").contains("My Cart (1)").should("be.visible");
   });
 });
